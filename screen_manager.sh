351053d53774010ba5d0e466fdfd6a717848728b
Onizuka09 <smokthar925@gmail.com>
2024-09-03 13:01:00 +0100

    Major change: - updated var syntax to ${var} - change the choice switch case implemantation - check on which display server is running (xorg or wayland) - check if (xrandr and mapfile) are installed

--- screen_manager.sh
+++ screen_manager.sh
@@ -1,17 +1,53 @@
 #!/bin/bash
 MAIN_SCR="eDP-1"
-# Run xrandr and capture its output
-XRANDR_OUTPUT=$(xrandr)
+
+XRANDR_CMD="xrandr"
+WAYLAND_CMD=""
+
+MAPFILE_CMD="mapfile"
+# Check which Display server you are using ( Wayland or Xorg )
+[[ $XDG_SESSION_TYPE == "x11" ]] && {
+  echo "Running on Xorg"
+  CMD=${XRANDR_CMD}
+  } || {
+    echo " Running on wayland " 
+    echo "Not supported for now "
+    echo "Exiting ..."
+    exit 1
+  }
+
+
+#Check if CMD exists
+[[ ! $(command -v ${CMD}  &> /dev/null )  ]] && {
+    # Run CMD and capture its output
+    CMD_OUTPUT=$(${CMD}) 
+    echo "xrandr installed ..."
+  } || {
+    echo "${CMD} not installed, Please installed :)"
+    echo "exiting ..."
+    exit 1
+  }
+
 # get all the screens detected by the xrandr 
-CONN_DISP_NAME=$(echo "$XRANDR_OUTPUT" | grep " connected ")
+CONN_DISP_NAME=$(echo "${CMD_OUTPUT}" | grep " connected ")
 # from the detected screens get theire associated prot  
 echo "[INFO]: Connected Display Ports:"
 #echo "$CONN_DISP" | awk '{print $1}'
-CONN_DISP_PORT=$(echo "$CONN_DISP_NAME" | awk '{print $1}')
-echo $CONN_DISP_PORT
+CONN_DISP_PORT=$(echo "${CONN_DISP_NAME}" | awk '{print $1}')
+echo ${CONN_DISP_PORT}
 echo "************" 
-# trasnform the CONN_DISP_PORT to an array called SCR_PORTS 
-mapfile -t SCR_PORTS <<< "$CONN_DISP_PORT"
+# check if mapfile command exist
+if  command -v ${MAPFILE_CMD} &> /dev/null
+then  
+  # trasnform the CONN_DISP_PORT to an array called SCR_PORTS 
+  ${MAPFILE_CMD} -t SCR_PORTS <<< "${CONN_DISP_PORT}"
+  echo $MAIN_SCR
+else 
+  echo "${MAPFILE_CMD} isn't installed in your system"
+  echo "please installed !" 
+  echo "exiting ..."
+  exit 1
+fi
 # loop through the array until it finds a new screen that is diffrent from the main screen {eDP-1}
 # @TODO: 
 # It will choose the first screen that the xrandr detected, so if you want to manage
@@ -19,21 +55,28 @@ mapfile -t SCR_PORTS <<< "$CONN_DISP_PORT"
 # for example: set the screen you cannot unless it's detected first by xrandr  
 for port in "${SCR_PORTS[@]}"
  do 
-    if [ "$port" != $MAIN_SCR ]; then 
+    if [ "${port}" != ${MAIN_SCR} ]; then 
       echo "[INFO]: new screen detected ${port}"
       NEW_SCR=${port} 
 	    break
     fi 
 done  
 # check if there is there is a connected screen
-echo $NEW_SCR
-if [[ -z $NEW_SCR ]]; then
+echo ${NEW_SCR}
+if [[ -z ${NEW_SCR} ]]; then
   ERR_STR="No screen detected, Please make sure you have set properly your screen !!"
   echo "[ERROR]: ${ERR_STR}" 
   echo "[ERROR]: Nothing to do existing ..."
   notify-send -a  -c "error" "[ERROR]: No screen detected" -t 3000 # ${ERR_STR} -t  
   exit 1
 fi 
+# Dict of choices
+declar -A SCREEN_CHOICES=(
+["Share Screen Left"]="--left-of"
+["Share Screen Right"] ="--right-of"
+["Duplicate Screen"]="--same-as"
+)
+XRANDR_CMD="xrandr --output"
 # set's up the dmenu choices  
 CHOICE=$(echo -e \
 "Share screen Left
@@ -43,27 +86,10 @@ Disable Screen"\
 | rofi -dmenu -p "Screen manager: ")
 
 # Process the user's choice
-case "$CHOICE" in
-    "Share screen Left") 
-        echo "[OPT-1]: SHARING SCREEN Left to MAIN screen."
-        xrandr --output $NEW_SCR  --auto --left-of $MAIN_SCR
-    ;;
-    "Share screen Right") 
-      echo "[OPT-2]: SHARING SCREEN Right to MAIN screen."
-      xrandr --output $NEW_SCR  --auto --right-of $MAIN_SCR
-    ;;
-    "Duplicate Screen")
-      echo "[OPT-3]: DUPLICATING screen"
-      xrandr --output $NEW_SCR --auto --same-as $MAIN_SCR
-    ;;
-    "Disable Screen")
-      echo "[OPT-4]: STOP SHARING screen."
- 	    xrandr --output $NEW_SCR --off
-    ;;
-    *) 
-      echo "[ERROR]: Invalid choice."; exit 1;;
-esac
+CHOICE=${SCREEN_CHOICES[${CHOICE}]}
 
- 
- #eDP-1
+[[ "${CHOICE}" == "--off" ]] && 
+  ${XRANDR_CMD} --off
+||
+  ${XRANDR_CMD} --outo ${CHOICE} ${MAIN_SCR}
 
