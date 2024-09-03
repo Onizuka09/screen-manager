e7201890f08a796cf216f71d999f31d76f83a2a3
Onizuka09 <smokthar925@gmail.com>
2024-09-03 13:22:00 +0100

    grouped instructions into functions

--- screen_manager.sh
+++ screen_manager.sh
@@ -4,8 +4,19 @@ MAIN_SCR="eDP-1"
 XRANDR_CMD="xrandr"
 WAYLAND_CMD=""
 
+
 MAPFILE_CMD="mapfile"
+
+# NOTE: to add a choice here, don't forget to add it in the rofi menu
+declare -A SCREEN_CHOICES=(
+["Share Screen Left"]="--left-of"
+["Share Screen Right"]="--right-of"
+["Duplicate Screen"]="--same-as"
+)
+
+
 # Check which Display server you are using ( Wayland or Xorg )
+check_display_server(){
 [[ $XDG_SESSION_TYPE == "x11" ]] && {
   echo "Running on Xorg"
   CMD=${XRANDR_CMD}
@@ -15,16 +26,18 @@ MAPFILE_CMD="mapfile"
     echo "Exiting ..."
     exit 1
   }
-
-
+}
+# returns the connected displays 
+#
+get_connected_displayes(){
 #Check if CMD exists
 [[ ! $(command -v ${CMD}  &> /dev/null )  ]] && {
     # Run CMD and capture its output
     CMD_OUTPUT=$(${CMD}) 
-    echo "xrandr installed ..."
+    # echo "xrandr installed ..."
   } || {
-    echo "${CMD} not installed, Please installed :)"
-    echo "exiting ..."
+    echo "[ERROR]: ${CMD} not installed, Please installed :)"
+    echo "[ERROR]: exiting ..."
     exit 1
   }
 
@@ -35,7 +48,10 @@ echo "[INFO]: Connected Display Ports:"
 #echo "$CONN_DISP" | awk '{print $1}'
 CONN_DISP_PORT=$(echo "${CONN_DISP_NAME}" | awk '{print $1}')
 echo ${CONN_DISP_PORT}
-echo "************" 
+}
+## returns the connected screen 
+get_new_screen_port(){
+
 # check if mapfile command exist
 if  command -v ${MAPFILE_CMD} &> /dev/null
 then  
@@ -43,13 +59,14 @@ then
   ${MAPFILE_CMD} -t SCR_PORTS <<< "${CONN_DISP_PORT}"
   echo $MAIN_SCR
 else 
-  echo "${MAPFILE_CMD} isn't installed in your system"
-  echo "please installed !" 
-  echo "exiting ..."
+  echo "[ERROR]: ${MAPFILE_CMD} isn't installed in your system"
+  echo "[ERROR]: please installed !" 
+  echo "[ERROR]: exiting ..."
   exit 1
 fi
+
 # loop through the array until it finds a new screen that is diffrent from the main screen {eDP-1}
-# @TODO: 
+# TODO: 
 # It will choose the first screen that the xrandr detected, so if you want to manage
 # 3 screens you won't be able at the first time
 # for example: set the screen you cannot unless it's detected first by xrandr  
@@ -70,13 +87,10 @@ if [[ -z ${NEW_SCR} ]]; then
   notify-send -a  -c "error" "[ERROR]: No screen detected" -t 3000 # ${ERR_STR} -t  
   exit 1
 fi 
+}
+set_choices(){
 # Dict of choices
-declar -A SCREEN_CHOICES=(
-["Share Screen Left"]="--left-of"
-["Share Screen Right"] ="--right-of"
-["Duplicate Screen"]="--same-as"
-)
-XRANDR_CMD="xrandr --output"
+XRANDR_CMD="${CMD} --output"
 # set's up the dmenu choices  
 CHOICE=$(echo -e \
 "Share screen Left
@@ -88,8 +102,14 @@ Disable Screen"\
 # Process the user's choice
 CHOICE=${SCREEN_CHOICES[${CHOICE}]}
 
-[[ "${CHOICE}" == "--off" ]] && 
+[[ "${CHOICE}" == "--off" ]] && { 
   ${XRANDR_CMD} --off
-||
-  ${XRANDR_CMD} --outo ${CHOICE} ${MAIN_SCR}
+}||{
+  ${XRANDR_CMD} ${NEW_SCR} --auto ${CHOICE} ${MAIN_SCR}
+}
+}
+
+check_display_server
+get_connected_displayes
+get_new_screen_port
 
